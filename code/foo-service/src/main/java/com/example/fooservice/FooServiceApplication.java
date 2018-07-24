50038f2dfb2fd8bff112492ccf118a0599041646
nam-duong <nam.duong.github@gmail.com>
2018-07-24 09:55:23 +0700

    add code folder

---
+++ code/foo-service/src/main/java/com/example/fooservice/FooServiceApplication.java
@@ -0,0 +1,22 @@
+package com.example.fooservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class FooServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooServiceApplication.class, args);
+	}
+
+	@GetMapping("/")
+    public String getFoo() {
+	    return "Foo service v1.0";
+    }
+}
